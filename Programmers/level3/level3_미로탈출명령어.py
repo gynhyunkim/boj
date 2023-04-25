c5a087a4df531e2789363364e6133f4432816e45
gynhyunkim <gyeonghyun.kimm@gmail.com>
2023-04-25 15:44:56 +0900

    add: 프로그래머스 level3 미로 탈출 명령어 풀이

---
+++ Programmers/level3/level3_미로탈출명령어.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(n, m, x, y, r, c, k):
+    answer = ''
+    q = deque([(x, y, "", 0)]) 
+    dt = [(1, 0, 'd'), (0, -1, 'l'), (0, 1, 'r'), (-1, 0, 'u')]
+    
+    while q:
+        x, y, path, cnt = q.popleft()
+        if (x, y) == (r, c) and (k - cnt) % 2 == 1 or abs(x - r) + abs(y - c) > k:
+            return "impossible"
+        if (x, y) == (r, c) and k == cnt:
+            return path
+        for i in range(4):
+            dx, dy, direction = dt[i]
+            nx = x + dx
+            ny = y + dy
+            if nx > 0 and nx <= n and ny > 0 and ny <= m and abs(nx - r) + abs(ny - c) + cnt + 1 <= k:
+                q.append((nx, ny, path + direction, cnt + 1))
+                break
+                
+    return answer
+
