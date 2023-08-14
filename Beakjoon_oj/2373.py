be33497d512545dff3f56cbf9e6a46fa66acfe71
gynhyunkim <gyeonghyun.kimm@gmail.com>
2023-08-14 20:41:58 +0900

    add: 백준 2373번 풀이

---
+++ Beakjoon_oj/2373.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+N = int(stdin.readline())
+fibo = [0, 1]
+for i in range(2, 75):
+    fibo.append(fibo[i - 2] + fibo[i - 1])
+    if fibo[i] >= N:
+        break
+
+if N == fibo[-1]:
+    print(-1)
+else:
+    for i in range(len(fibo) - 1, 2, -1):
+        if fibo[i] > N:
+            continue
+        elif fibo[i] == N:
+            break
+        N -= fibo[i]
+    print(N)
